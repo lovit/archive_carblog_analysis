cc64d3fab382f9c0fcd45a254caf60e69bb81a49
lovit <soy.lovit@gmail.com>
2019-07-05 18:29:31 +0900

    Use soynlp

--- carblog_analyzer/__init__.py
+++ carblog_analyzer/__init__.py
@@ -1,2 +1,8 @@
-__version__ = 0.0.1
+__version__ = '0.0.1'
+__author__ = 'lovit'
 
+try:
+    import soynlp
+    print('Available soynlp == {}'.format(soynlp.__version__))
+except:
+    print('First install soynlp using "pip install soynlp" or "git clone https://github.com/lovit/soynlp.git"')
